95d92fc93a6a1e628b4f8a6c663ab6f8911d0e1c
Royserg <staniojakub@gmail.com>
2023-11-06 20:10:00 +0300

    chore: generate Nx workspace [NestJS, React, tailwindcss]

---
+++ apps/frontend/src/app/app.tsx
@@ -0,0 +1,11 @@
+import "./main.css";
+
+export function App() {
+  return (
+    <main className="flex h-screen w-screen items-center justify-center border-2 bg-slate-200">
+      <h1 className="text-4xl font-bold text-black">Frontend</h1>
+    </main>
+  );
+}
+
+export default App;
