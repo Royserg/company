3f07d416a81afacbe1251ddf2f76ba7f3bff771b
Royserg <staniojakub@gmail.com>
2023-11-07 20:42:02 +0300

    test(api): add unit tests - controller 'createNode'

--- apps/api/src/app/app.controller.spec.ts
+++ apps/api/src/app/app.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Node } from './interfaces/node';
+import { CreateNodeDto } from './dto/create-node.dto';
 
 describe('AppController', () => {
   let app: TestingModule;
@@ -16,6 +17,7 @@ describe('AppController', () => {
           provide: AppService,
           useValue: {
             getNodeChildren: jest.fn(),
+            createNode: jest.fn(),
           },
         },
       ],
@@ -32,8 +34,43 @@ describe('AppController', () => {
     height: 0,
   };
 
+  describe('createNode', () => {
+    it(`should call service method 'createNode' with dto`, () => {
+      const createNodeDto: CreateNodeDto = {
+        name: 'NODE_NAME',
+        parentId: 'PARENT_ID',
+      };
+
+      jest.spyOn(appService, 'createNode');
+
+      appController.createNode(createNodeDto);
+
+      expect(appService.createNode).toHaveBeenCalledTimes(1);
+      expect(appService.createNode).toHaveBeenCalledWith(createNodeDto);
+    });
+
+    it(`should return newly created Node`, () => {
+      const createNodeDto: CreateNodeDto = {
+        name: 'NODE_NAME',
+        parentId: 'PARENT_ID',
+      };
+      const newNode: Node = {
+        id: 'NODE_ID',
+        name: 'NODE_NAME',
+        parentId: 'PARENT_ID',
+        height: 1,
+      };
+
+      jest.spyOn(appService, 'createNode').mockReturnValue(newNode);
+
+      const result = appController.createNode(createNodeDto);
+
+      expect(result).toEqual(newNode);
+    });
+  });
+
   describe('getNodeChildren', () => {
-    it(`should call service method 'getNodeChildren' passing nodeId`, () => {
+    it(`should call service method 'getNodeChildren' with nodeId`, () => {
       const nodeId = 'NODE_ID';
 
       jest.spyOn(appService, 'getNodeChildren');
