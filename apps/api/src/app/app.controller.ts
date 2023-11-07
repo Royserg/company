f52246b659220f9ac21e803626299c2c1864c8aa
Royserg <staniojakub@gmail.com>
2023-11-07 20:42:21 +0300

    feat(api): add endpoint for 'createNode'

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateNodeDto } from './dto/create-node.dto';
 
-@Controller()
+@Controller('nodes')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Post()
+  createNode(@Body() createNodeDto: CreateNodeDto) {
+    return this.appService.createNode(createNodeDto);
+  }
 
   @Get(':nodeId/children')
   getNodeChildren(@Param('nodeId') nodeId: string) {
