f5365a955d8fcb0329f5fcac8a0024dad9b1e155
Royserg <staniojakub@gmail.com>
2023-11-07 19:42:01 +0300

    test(api): add unit tests - service method 'getNodeChildren'

--- apps/api/src/app/app.service.spec.ts
+++ apps/api/src/app/app.service.spec.ts
@@ -1,4 +1,17 @@
 import { Test } from '@nestjs/testing';
+import {
+  mockMemoryDb,
+  mockRoot,
+  mockA,
+  mockB,
+  mockC,
+  mockD,
+  mockE,
+} from './mocks/mock-memory-db';
+
+jest.mock('./data/memory-db', () => ({
+  memoryDb: mockMemoryDb,
+}));
 
 import { AppService } from './app.service';
 
@@ -13,9 +26,40 @@ describe('AppService', () => {
     service = app.get<AppService>(AppService);
   });
 
-  describe('getData', () => {
-    it('should return "Hello API"', () => {
-      expect(service.getData()).toEqual({message: 'Hello API'});
+  describe('getNodeChildren', () => {
+    it('should return 2 children of root node: [A, B]', () => {
+      const result = service.getNodeChildren(mockRoot.id);
+      const expected = [mockA, mockB];
+
+      expect(result).toEqual(expected);
+    });
+
+    it('should return 1 child of A node: [C]', () => {
+      const result = service.getNodeChildren(mockA.id);
+      const expected = [mockC];
+
+      expect(result).toEqual(expected);
+    });
+
+    it('should return 2 children of C node: [D, E]', () => {
+      const result = service.getNodeChildren(mockC.id);
+      const expected = [mockD, mockE];
+
+      expect(result).toEqual(expected);
+    });
+
+    it('should return 0 children of D node: []', () => {
+      const result = service.getNodeChildren(mockD.id);
+      const expected = [];
+
+      expect(result).toEqual(expected);
+    });
+
+    it('should return 0 children of E node: []', () => {
+      const result = service.getNodeChildren(mockE.id);
+      const expected = [];
+
+      expect(result).toEqual(expected);
     });
   });
 });
