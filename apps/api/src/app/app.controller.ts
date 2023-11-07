35f8c39e17a19379b54ca706e509983e9fb9d778
Royserg <staniojakub@gmail.com>
2023-11-07 19:41:05 +0300

    feat(api): add endpoint for 'getNodeChildren'

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,13 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @Get(':nodeId/children')
+  getNodeChildren(@Param('nodeId') nodeId: string) {
+    return this.appService.getNodeChildren(nodeId);
   }
 }
