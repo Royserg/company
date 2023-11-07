c2480bbf6d9c126773bece61679da98bcd51945f
Royserg <staniojakub@gmail.com>
2023-11-07 19:40:43 +0300

    test(api): add unit tests - controller 'getNodeChildren'

--- apps/api/src/app/app.controller.spec.ts
+++ apps/api/src/app/app.controller.spec.ts
@@ -1,22 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Node } from './interfaces/node';
 
 describe('AppController', () => {
   let app: TestingModule;
+  let appController: AppController;
+  let appService: AppService;
 
   beforeAll(async () => {
     app = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        {
+          provide: AppService,
+          useValue: {
+            getNodeChildren: jest.fn(),
+          },
+        },
+      ],
     }).compile();
+
+    appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
   });
 
-  describe('getData', () => {
-    it('should return "Hello API"', () => {
-      const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({message: 'Hello API'});
+  const mockNode: Node = {
+    id: 'NODE_ID',
+    name: 'Node',
+    parentId: null,
+    height: 0,
+  };
+
+  describe('getNodeChildren', () => {
+    it(`should call service method 'getNodeChildren' passing nodeId`, () => {
+      const nodeId = 'NODE_ID';
+
+      jest.spyOn(appService, 'getNodeChildren');
+
+      appController.getNodeChildren(nodeId);
+
+      expect(appService.getNodeChildren).toHaveBeenCalledTimes(1);
+      expect(appService.getNodeChildren).toHaveBeenCalledWith(nodeId);
+    });
+
+    it(`should return list of Nodes`, () => {
+      const nodeId = 'NODE_ID';
+      jest.spyOn(appService, 'getNodeChildren').mockReturnValue([mockNode]);
+
+      const result = appController.getNodeChildren(nodeId);
+      const expected = [mockNode];
+
+      expect(result).toEqual(expected);
     });
   });
 });
