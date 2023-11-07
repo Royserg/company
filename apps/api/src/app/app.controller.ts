c95d76be370fd6abe92ac4e6fb0af618679f719f
Royserg <staniojakub@gmail.com>
2023-11-07 21:32:11 +0300

    refactor(api): add return types to controller endpoints

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,18 +1,19 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CreateNodeDto } from './dto/create-node.dto';
+import { Node } from './interfaces/node';
 
 @Controller('nodes')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post()
-  createNode(@Body() createNodeDto: CreateNodeDto) {
+  createNode(@Body() createNodeDto: CreateNodeDto): Node {
     return this.appService.createNode(createNodeDto);
   }
 
   @Get(':nodeId/children')
-  getNodeChildren(@Param('nodeId') nodeId: string) {
+  getNodeChildren(@Param('nodeId') nodeId: string): Node[] {
     return this.appService.getNodeChildren(nodeId);
   }
 }
