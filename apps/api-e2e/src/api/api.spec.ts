bbf22ba7680b75610c69cd529a3ff7e0d1a3d426
Royserg <staniojakub@gmail.com>
2023-11-08 09:08:22 +0300

    test(api-e2e): add simple test

--- apps/api-e2e/src/api/api.spec.ts
+++ apps/api-e2e/src/api/api.spec.ts
@@ -1,10 +1,27 @@
 import axios from 'axios';
 
-describe('GET /api', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/api`);
+// NOTE: requires the server to be running (`nx serve api`)
 
-    expect(res.status).toBe(200);;
-    expect(res.data).toEqual({ message: 'Hello API' });
+describe('GET /api/nodes/root/children', () => {
+  it('should return a list with nodes', async () => {
+    const res = await axios.get(`/api/nodes/root/children`);
+
+    const expected = [
+      {
+        height: 1,
+        id: 'b74770b1-a760-43e2-aeb9-6d389104ef6f',
+        name: 'A',
+        parentId: 'root',
+      },
+      {
+        height: 1,
+        id: '1966520a-66b3-44eb-a1ba-b71bd642bab8',
+        name: 'B',
+        parentId: 'root',
+      },
+    ];
+
+    expect(res.status).toBe(200);
+    expect(res.data).toEqual(expected);
   });
-})
+});
